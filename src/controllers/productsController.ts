5bfe2581d32bf247409bf2322e461cabbff4819e
Edson Rodrigues <thecodeverse12@gmail.com>
2025-04-26 01:23:56 +0100

    feat: products controller added

---
+++ src/controllers/productsController.ts
@@ -0,0 +1,52 @@
+import { supabase } from "@/utils/supabase";
+
+export class ProductsController {
+    static async getAllProducts() {
+        const { data, error } = await supabase
+            .from('products')
+            .select('*');
+
+        if (error) throw error;
+        return data;
+    }
+
+    static async getProductById(id: string) {
+        const { data, error } = await supabase
+            .from('products')
+            .select('*')
+            .eq('id', id)
+            .single();
+
+        if (error) throw error;
+        return data;
+    }
+
+    static async createProduct(name: string, category: string, description: string, price: number, image_url: string) {
+        const { error } = await supabase
+            .from('products')
+            .insert([{
+                name: name,
+                description: description,
+                price: price,
+                image_url: image_url,
+                category: category
+            }]);
+
+        if (error) throw error;
+    }
+
+    static async updateProduct(id: string, name?: string, category?: string, description?: string, price?: number, image_url?: string) {
+        const { error } = await supabase
+            .from('products')
+            .update({
+                name: name,
+                description: description,
+                price: price,
+                image_url: image_url,
+                category: category
+            })
+            .eq('id', id);
+
+        if (error) throw error;
+    }
+}
